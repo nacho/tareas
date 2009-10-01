e602a1193581e7c217fba6e0c268a6484f419058
Ignacio Casal Quinteiro <icq@gnome.org>
2009-10-01 19:07:18 +0200

    Add sql file for the database.

---
+++ tareas.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 01, 2009 at 07:06 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.2.10-2ubuntu4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `tareas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `idTask` int(11) NOT NULL AUTO_INCREMENT,
+  `IdUser` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `week` varchar(20) NOT NULL,
+  PRIMARY KEY (`idTask`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `task`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user`
+--
+
